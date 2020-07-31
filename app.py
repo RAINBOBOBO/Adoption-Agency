0dbb85c9fd59ea856697533184881a2678079b23
Rain Babauta <rainbabauta1@gmail.com>
2020-07-31 12:25:41 -0700

    Created flask form for adding pets, db model for pets, routes for root and /add, and html files for base and listing pets.

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 """Flask app for adopt app."""
 
-from flask import Flask
+from flask import Flask, render_template
 
 from flask_debugtoolbar import DebugToolbarExtension
 
 from models import db, connect_db
 
+from forms import AddPetForm
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "secret"
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///adopt"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///adopt"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:qwerty@localhost/adopt"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 connect_db(app)
@@ -22,3 +25,16 @@ db.create_all()
 # app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 toolbar = DebugToolbarExtension(app)
+
+
+@app.route("/")
+def root():
+  pets = Pet.query.all()
+  return render_template("list-pets.html", pets=pets)
+
+@app.route("/add", methods=["GET", "POST"])
+def add_form():
+
+  form = AddPetForm()
+
+  if form.validate_on_submit
--- models.py
+++ models.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+DEFAULT_IMAGE_URL = 'https://cdn.dribbble.com/users/2095589/screenshots/4166422/user_1.png'
+
 
 def connect_db(app):
     """Connect this database to provided Flask app.
@@ -13,3 +15,21 @@ def connect_db(app):
 
     db.app = app
     db.init_app(app)
+
+
+class Pet(db.Model):
+    __tablename__ = 'pets'
+
+    id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+    name = db.Column(db.String(30), nullable=False)
+    species = db.Column(db.String(30), nullable=False)
+    photo_url = db.Column(db.String(250), nullable=False, default=DEFAULT_IMAGE_URL)
+    age = db.Column(db.String(30), nullable=False) #make a dropdown for this one
+    notes = db.Column(db.String(300))
+    available = db.Column(db.Boolean(), nullable=False, default=True)
+
+    def __repr__(self):
+    """Show info about the pet."""
+
+    p = self
+    return f"<Per {p.name} {p.species} {p.photo_url} {p.age} {p.notes} {p.available}>"
